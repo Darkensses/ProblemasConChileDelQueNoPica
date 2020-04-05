a735ce23874e744cefa7a736c1d48091c4371ea2
Laborico <david97132@hotmail.com>
2020-04-05 15:24:05 -0500

    B optimzado para python

--- Laborico/Code Jam 2020/Qualification Round/B.py
+++ Laborico/Code Jam 2020/Qualification Round/B.py
@@ -1,21 +1,20 @@
+inp=input
+iinp= lambda: int(inp())
+o=ord
 def resolver():
-    s=input()
+    s=inp()
     res=''
-    cont=0
+    profundidad=0
     for i in s:
-        n=ord(i)-48
-        while cont<n:
-            res+='('
-            cont+=1
-        while cont>n:
-            res+=')'
-            cont-=1
+        n=o(i)-48
+        res+='('*(n-profundidad)
+        res+=')'*(profundidad-n)
         res+=i
-    for i in range(cont):
-        res+=')'
+        profundidad=n
+    res+=')'*profundidad
     return res
      
-t=int(input())
+t=iinp()
 for i in range(t):
     res=resolver()
     print("Case #%i: %s"%(i+1,res))
